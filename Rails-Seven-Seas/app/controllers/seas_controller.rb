28adfb825fb78b4b2c77126aa751e889875d0203
Jowita <jowita.lapies@gmail.com>
2019-06-27 16:58:34 +0100

    first sea

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,37 @@
 class SeasController < ApplicationController
   #define your controller actions here
+   def index 
+    @seas = Sea.all
+   end 
+
+   def show 
+    @sea = Sea.find(params[:id])
+   end
+
+   def new 
+     @sea = Sea.new
+   end
+
+   def create 
+      @sea = Sea.create(sea_params)
+      redirect_to @sea
+   end
+   
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update_attributes(sea_params)
+    redirect_to @sea
+    
+  end
+
+  def destroy
+    @sea = Sea.destroy(params[:id])
+    redirect_to @sea 
+  end
 
 
   private
