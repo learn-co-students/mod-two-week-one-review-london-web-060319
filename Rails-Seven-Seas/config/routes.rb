1b134c1727f89d615dd1bf6d6c9e8cc560556319
Louise Norris <louisenorris73@yahoo.co.uk>
2019-06-27 18:07:08 +0100

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :seas
+
+# , :only [:index, :create]
+#
+# resources :'/seas/new', :only :new
+#
+# resources :'/seas/:id', :only [:show, :update, :destroy]
+#
+# resources :'/seas/:id/edit', only: :edit
+
 end
