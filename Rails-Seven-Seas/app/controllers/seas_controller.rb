54fb481b60813a42602b810d6151606322d4ddc7
Scott <galejscott@gmail.com>
2019-06-27 17:45:48 +0100

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,37 @@
 class SeasController < ApplicationController
+  skip_before_action :verify_authenticity_token
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
 
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to @sea
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    redirect_to @sea
+  end
+
+  def destroy
+    Sea.destroy(params[:id])
+    redirect_to "/seas/"
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
